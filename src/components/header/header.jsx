26cdb19c8261bb733c5029024539dec4dfe16a4b
Vladimir <128.valdemar@gmail.com>
2023-05-19 18:24:49 +0300

    v1.6

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import SearchIcon from "../svgs/searchIcon";
 
 import "./header.css";
 
 
-export default function Header({ setCurrentCity, isIncorrect }) {
+export default function Header({setCurrentCity, isIncorrect}) {
 
-    let [inputText, setInputText] = useState("");
+    const [inputText, setInputText] = useState("");
+
+
+    const ref = useRef(null);
 
 
     useEffect(() => {
-        document.addEventListener("keypress", e => {
-            if (e.key === "Enter") setCurrentCity(inputText);
-        });
+        ref.current.focus();
     }, []);
+
+
+    const onInputChange = (e) => {
+        setInputText(e.target.value);
+        if (e.key === "Enter") setCurrentCity(inputText);
+        console.log(e);
+    }
 
 
     return (
         <div className="header">
             <div className="header__input">
-                <input placeholder="City or country..." type="text" onInput={e => setInputText(e.target.value)} />
+                <input ref={ref} placeholder="City or country..." type="text" onKeyDown={onInputChange} autoFocus />
             </div>
             <div className="header__search-btn">
                 <button onClick={() => setCurrentCity(inputText)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 72 72">
-                        <path d="M 31 11 C 19.973 11 11 19.973 11 31 C 11 42.027 19.973 51 31 51 C 34.974166 51 38.672385 
-                        49.821569 41.789062 47.814453 L 54.726562 60.751953 C 56.390563 62.415953 59.088953 62.415953 60.751953 
-                        60.751953 C 62.415953 59.087953 62.415953 56.390563 60.751953 54.726562 L 47.814453 41.789062 C 
-                        49.821569 38.672385 51 34.974166 51 31 C 51 19.973 42.027 11 31 11 z M 31 19 C 37.616 19 43 24.384 
-                        43 31 C 43 37.616 37.616 43 31 43 C 24.384 43 19 37.616 19 31 C 19 24.384 24.384 19 31 19 z" />
-                    </svg>
+                    <SearchIcon />
                 </button>
             </div>
             <div className={`incorrect${isIncorrect ? " active" : ""}`}>Incorrect city or country</div>
